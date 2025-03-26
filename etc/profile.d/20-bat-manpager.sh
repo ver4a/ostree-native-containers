9ab7accb63466459f43475b170c89a92adb9239d
ver4a <ver4a@uncontrol.me>
2025-03-26 13:56:30 +0100

    Fix paging when LESS variable is set

--- etc/profile.d/20-bat-manpager.sh
+++ etc/profile.d/20-bat-manpager.sh
@@ -1,2 +1,2 @@
-export MANPAGER="sh -c 'col -bx | bat -l man --decorations=never --paging=always --pager \"less -RFK\"'"
+export MANPAGER="sh -c 'col -bx | LESS="RK" bat -l man --decorations=never --paging=always'"
 export MANROFFOPT="-c"
